7f135036e293e39aca6ec84471f4f1610589089c
Nicholas Auer <nick.nickauer@gmail.com>
2018-12-06 18:00:37 -0700

    handle failed logins

--- main.js
+++ main.js
@@ -75,6 +75,9 @@ app.post('/verify', function (req, res) {
             else {
                 res.redirect("login.html");
             }
+        })
+        .catch(function () {
+            res.redirect("login.html");
         });
     }
     else {
